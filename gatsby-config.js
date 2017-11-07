fd104995226c9ba7a21204bff3398afebee46222
Tim Arney <tim@line37.com>
2017-11-07 05:06:51 -0500

    add manifest support

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,33 @@ module.exports = {
         path: `${__dirname}/src/img/`
       }
     },
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "line37",
+        short_name: "line37",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#474747",
+        display: "minimal-ui",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
+      }
+    }
   ]
 };
