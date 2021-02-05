ec35de156ba1acd39b6c78c4ab62ea296ca4fb60
Tim Arney <timarney@users.noreply.github.com>
2021-02-05 14:13:12 -0500

    Update redirect.js

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -5,7 +5,11 @@ const RedirectPage = () => {
   
 
   return (
-   <div><a href="https://play.google.com/store/apps/details?id=ca.gc.hcsc.canada.stopcovid" rel="nofollow"><img src="https://camo.githubusercontent.com/01bfce275db21420f68311e5cb8893db38a4f7431bd6cf3a9646d2dad77ea004/68747470733a2f2f7777772e63616e6164612e63612f636f6e74656e742f64616d2f706861632d617370632f696d616765732f73657276696365732f64697365617365732d6d616c61646965732f636f726f6e6176697275732d646973656173652d636f7669642d31392f636f7669642d616c6572742f676f6f676c652d706c61792d656e672e706e67" alt="Get it on Google Play" data-canonical-src="https://www.canada.ca/content/dam/phac-aspc/images/services/diseases-maladies/coronavirus-disease-covid-19/covid-alert/google-play-eng.png" style="max-width:100%;"></a></div>
+   <div>
+    <a href="https://play.google.com/store/apps/details?id=ca.gc.hcsc.canada.stopcovid">
+      <img src="https://www.canada.ca/content/dam/phac-aspc/images/services/diseases-maladies/coronavirus-disease-covid-19/covid-alert/google-play-eng.png" alt="Get it on Google Play" />
+    </a>
+   </div>
   );
 };
 
