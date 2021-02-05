6503cdf6a2e8f18c9dc09d8cb855024246ae7f3f
Tim Arney <timarney@users.noreply.github.com>
2021-02-05 14:03:39 -0500

    Update redirect.js

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -5,9 +5,7 @@ const RedirectPage = () => {
   
 
   return (
-    <div>
-      Hello
-    </div>
+    <meta http-equiv="refresh" content="0;url=https://play.google.com/store/apps/details?id=ca.gc.hcsc.canada.stopcovid" />
   );
 };
 
